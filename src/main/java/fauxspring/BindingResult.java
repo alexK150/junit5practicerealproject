0ea7ce1167c5a22046e7902922cdc0817a826919
Alex <akuzin20@gmail.com>
2020-03-17 13:55:30 +0300

    Initial commit

---
+++ src/main/java/fauxspring/BindingResult.java
@@ -0,0 +1,7 @@
+package fauxspring;
+
+public interface BindingResult {
+    void rejectValue(String lastName, String notFound, String not_found);
+
+    boolean hasErrors();
+}
---
+++ src/main/java/fauxspring/ModelAndView.java
@@ -0,0 +1,13 @@
+package fauxspring;
+
+public class ModelAndView {
+
+    public ModelAndView() {
+    }
+
+    public ModelAndView(String view) {
+    }
+
+    public void addObject(Object o){}
+
+}
