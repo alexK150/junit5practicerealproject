f606776cb706d00bf69a560b140ead089d600740
Alex <akuzin20@gmail.com>
2020-03-17 15:02:07 +0300

    commit v4 dependent assertion done

---
+++ src/test/java/model/OwnerTest.java
@@ -0,0 +1,25 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OwnerTest {
+
+    @Test
+    void dependentAssertions() {
+        Owner owner = new Owner(1L, "John", "Doe");
+        owner.setCity("New York");
+        owner.setTelephone("1111-111-11");
+
+        assertAll("Assert of all props",
+                () -> assertAll("Person Props",
+                        () -> assertEquals("John", owner.getFirstName(), "First Name is wrong"),
+                        () -> assertEquals("Doe", owner.getLastName(), "Last Name is wrong")),
+                () -> assertAll("Owner Props",
+                        () -> assertEquals("New York", owner.getCity(), "City is wrong"),
+                        () -> assertEquals("1111-111-11", owner.getTelephone(), "Phone number is wrong")
+                )
+        );
+    }
+}
